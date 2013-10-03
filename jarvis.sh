dd1f986a0f10ff79c8fa49542765e3ed1af5e248
gskielian <gregory.kielian@hackerdojo.com>
2013-10-03 00:40:21 -0700

    Update jarvis.sh

--- jarvis.sh
+++ jarvis.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-say "you said " $1
+clear
+echo "Jarvis Online" | figlet
+read message
+
+if [ "$message" = "mailbox" ] ; then
+  open http://gmail.com
+  sleep 4
+elif [ "$message" = "drive" ] ; then
+  open http://drive.google.com
+  sleep 4
+elif [ "$message" = "keep" ] ; then
+  open http://www.google.com/keep
+  sleep 4
+elif [ "$message" = "hacker news" ] ; then
+  open http://news.ycombinator.com
+  sleep 4
+elif [ "$message" = "Time" ] ; then
+  osascript -e "set volume 5"
+  say `date +%r`
+  osascript -e "set volume 0"
+fi
+
