b2472fd1942c91b58bf28e8ba080f95165fb96a4
gskielian <gregory.kielian@hackerdojo.com>
2013-10-08 00:00:48 -0700

    Created play_a_tune.sh described how to use
    can either open with itunes or vlc

---
+++ play_a_tune.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+### Usage, download VLC, and find out where it is, or you can use itunes
+
+### This is how I got vlc to work on my computer
+###/Applications/VLC.app/Contents/MacOS/VLC ~/Music/your_music_file.mp3
+
+### This is just an itunes implementation
+open ~/Music/your_music_file.mp3
